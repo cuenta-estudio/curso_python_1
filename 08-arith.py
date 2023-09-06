638582489e06dce5e003f75d67c7b3edcd3502e6
Matias Melo Estudio <matias.melo.estudio@gmail.com>
2023-09-05 21:03:44 -0300

    De la 08 a la 12

---
+++ 08-arith.py
@@ -0,0 +1,20 @@
+print(10 + 10)
+print(10 - 5)
+print(10 * 2)
+print(10 / 2) # 5
+print(10 % 2) # 0
+print(10 / 3)
+print(10 % 3)
+## // division dejando solo el numero entero
+print(10 // 3)
+## ** elevado a 
+print(2 ** 3)
+print(2 ** 3 + 3 - 7 / 1 // 4)
+print(2 ** 3)
+print((7 / 1) // 4)
+
+print(8 + 3 - 1)
+print(10 / 0)
+
+print('Hola' + ' mundo')
+print('Hola' * 3)
